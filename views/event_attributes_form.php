efcc37464b7449ccf6bf4be1367ee231ac00750e
Jonathan Hartman <jhartman86@gmail.com>
2015-09-09 17:56:30 -0600

    Updates to attr form

--- views/event_attributes_form.php
+++ views/event_attributes_form.php
@@ -1,16 +1,21 @@
 <?php /** @var $eventObj \Concrete\Package\Schedulizer\Src\Event */
 $attrList = \Concrete\Package\Schedulizer\Src\Attribute\Key\SchedulizerEventKey::getList();
+$chunked  = array_chunk($attrList, 2);
 
-if( !empty($attrList) ): foreach($attrList AS $attrKeyObj){ ?>
+
+
+if( !empty($attrList) ): foreach($chunked AS $pair): ?>
     <div class="row">
-        <div class="col-sm-12">
-            <div class="form-group">
-                <label><?php echo $attrKeyObj->getAttributeKeyName(); ?></label>
-                <?php echo $attrKeyObj->render('form', $eventObj->getAttributeValueObject($attrKeyObj), true); ?>
+        <?php foreach($pair AS $attrKeyObj){ /** @var $attrKeyObj \Concrete\Core\Attribute\Key\Key */ ?>
+            <div class="col-sm-6">
+                <div class="form-group" data-type="<?php echo $attrKeyObj->getAttributeTypeHandle(); ?>">
+                    <label><?php echo $attrKeyObj->getAttributeKeyName(); ?></label>
+                    <?php echo $attrKeyObj->render('form', $eventObj->getAttributeValueObject($attrKeyObj), true); ?>
+                </div>
             </div>
-        </div>
+        <?php } ?>
     </div>
-<?php } else: ?>
+<?php endforeach; else: ?>
     <div class="row">
         <div class="col-sm-12">
             <p class="lead text-center">No Custom Attributes Setup Yet</p>
